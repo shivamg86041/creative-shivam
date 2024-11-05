058c4cdcc1901adaa5bf43c52f67901fd3103b08
shivamg86041 <shivamgupta86041@gmail.com>
2024-11-05 23:09:11 +0530

    Updated projects

--- src/components/Work/Data.jsx
+++ src/components/Work/Data.jsx
@@ -1,56 +1,63 @@
-import Work1 from "../../assets/work1.png"
-import Work2 from "../../assets/work2.png"
-import Work3 from "../../assets/work3.png"
-import Work4 from "../../assets/work4.png"
-import Work5 from "../../assets/work5.png"
+import Work1 from "../../assets/work1.png";
+import Work2 from "../../assets/work2.png";
+import Work3 from "../../assets/work3.png";
+import Work4 from "../../assets/work4.png";
+import Work5 from "../../assets/work5.png";
+import Work6 from "../../assets/Work6.png";
 
 export const projectsData = [
-    {
-      id: 1,
-      image: Work1,
-      title: "BTech Burger Wala",
-      category: "web",
-      link : "https://btech-burger-wala.netlify.app/",
-    },
-    {
-      id: 3,
-      image: Work2,
-      title: "ShivGPT",
-      category: "design",
-      link: "https://shivgpt.netlify.app/",
-    },
-    {
-      id: 4,
-      image: Work3,
-      title: "Talk-A-Tive",
-      category: "web",
-      link: "https://talk-a-tive.netlify.app/",
-    },
-    {
-      id: 5,
-      image: Work4,
-      title: "Hirrd",
-      category: "web",
-      link: "https://hirrd.vercel.app/",
-    },
-    {
-      id: 6,
-      image: Work5,
-      title: "Trimmr",
-      category: "web",
-      link: "https://trimmr0.netlify.app/",
-    },
-  ];
+  {
+    id: 1,
+    image: Work6,
+    title: "NetflixGPT - AI Powered",
+    category: "web",
+    link: "https://netflixgpt01.vercel.app/",
+  },
+  {
+    id: 2,
+    image: Work1,
+    title: "BTech Burger Wala",
+    category: "web",
+    link: "https://btech-burger-wala.netlify.app/",
+  },
+  {
+    id: 3,
+    image: Work2,
+    title: "ShivGPT",
+    category: "design",
+    link: "https://shivgpt.netlify.app/",
+  },
+  {
+    id: 4,
+    image: Work3,
+    title: "Talk-A-Tive",
+    category: "web",
+    link: "https://talk-a-tive.netlify.app/",
+  },
+  {
+    id: 5,
+    image: Work4,
+    title: "Hirrd",
+    category: "web",
+    link: "https://hirrd.vercel.app/",
+  },
+  {
+    id: 6,
+    image: Work5,
+    title: "Trimmr",
+    category: "web",
+    link: "https://trimmr0.netlify.app/",
+  },
+];
 
-  export const projectsNav = [
-    {
-        name: "1",
-    },
-    {
-        name: "2",
-    },
-    {
-        name: "3",
-    },
-  ]
-  
+export const projectsNav = [
+  {
+    name: "1",
+  },
+  {
+    name: "2",
+  },
+  {
+    name: "3",
+  },
+];
